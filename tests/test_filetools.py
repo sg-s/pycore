71fc2080751c6574bd1b9a7fde12c5f992c4f776
Srinivas Gorur-Shandilya <code@srinivas.gs>
2023-03-23 11:21:58 -0400

    removed a test that would clearly fail on remote

--- tests/test_filetools.py
+++ tests/test_filetools.py
@@ -1,15 +1,7 @@
-from pycore.filetools import format_bytes, get_folder_size
-
-
-def test_get_folder_size():
-
-    assert (
-        get_folder_size("/Users/srinivas/code/pycore/.github/") == 890
-    ), "folder size estimation failed"
+from pycore.filetools import format_bytes
 
 
 def test_format_bytes():
-
     assert (
         format_bytes(100_000_000_000_000) == "90.95 TB"
     ), "Format bytes failed"
